50faf8286fa4d9e627c13bc40fb1b016339676c5
Naman Keshari <naman.manjul@gmail.com>
2021-08-10 16:46:07 +0530

    changed index

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Layout from "../components/layout";
 const IndexPage = () => {
-  return <Layout title="home page">test</Layout>;
+  return <Layout title="Chat app">Home Page</Layout>;
 };
 export default IndexPage;
